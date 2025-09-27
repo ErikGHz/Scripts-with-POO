2842e4454df9d9cca188509ca70e44dbe4f85913
ErikGH <erikguzh@gmail.com>
2025-09-27 00:33:32 -0600

    Se agregó Verificar Usuario y Verificar Base de datos en los metodos de la clase BasedeDatos

--- createDatabase.py
+++ createDatabase.py
@@ -1,4 +1,5 @@
 import string, mysql.connector, os, time
+from mysql.connector import Error
 
 class BasedeDatos:
     def __init__(self) -> None:
@@ -8,41 +9,54 @@ class BasedeDatos:
         self.db = None
         self.cursor = None
 
-    def conectar_servidor(self) -> None:
+    def conectarServidor(self) -> None:
         self.db = mysql.connector.connect(
             host = self.host,
             user = self.user,
             password = self.password
         )
-        if self.db.is_connected():
+        if self.db is not None and self.db.is_connected():
             print(f"Conectado correctamente a {self.host}")
             self.cursor = self.db.cursor()
         else:
-            print(f"No se ha podido conectar {self.host}")
-    
-    def verificar_bd(self, nombre) -> bool:
-        if self.db is None or not self.db.is_connected():
-            print("No te has conectado a una base de datos")
-            return False
-        
-        else:
-            #Si la base de datos existe es 1, si no existe sera 0
-            self.cursor.execute(f"SELECT IF(COUNT(*) > 0, 1, 0) FROM information_schema.schemata WHERE SCHEMA_NAME = '{nombre}'")
-            for verify in self.cursor:
-                print(verify[0])
-                return bool(verify[0])
+            print("La conexión con la base de datos no está activa.")
             
-    def crear_bd(self, nombre) -> None:
-        nombre = nombre
-        self.conectar_servidor()
-        self.verificar_bd(nombre)
-        self.cursor.execute(f"CREATE DATABASE IF NOT EXISTS {nombre};")
+    def verificarBD(self, baseDatos) -> bool:
+        if self.db is not None and self.db.is_connected():
+            self.cursor.execute(f"SELECT IF(COUNT(*) > 0, 1, 0) FROM information_schema.schemata WHERE SCHEMA_NAME = '{baseDatos}'")
+            #Si la base de datos existe es 1 (TRUE), si no existe sera 0 (FALSE)
+            for contenido in self.cursor:
+                valor = bool(contenido[0])
+            return valor
+        else:
+            print("No te has conectado a una base de datos")
+            return None
+            
+    def verificarUsuario(self, usuario) -> bool:
+        if self.db is not None and self.db.is_connected():
+            self.cursor.execute(f"SELECT IF(COUNT(*) > 0, 1, 0) FROM MYSQL.USER WHERE USER = '{usuario}'")\
+            #Si la base de datos existe es 1 (TRUE), si no existe sera 0 (FALSE)
+            for contenido in self.cursor:
+                valor = bool(contenido[0])
+            return valor
+        else:
+            print("No te has conectado a una base de datos")
+            return None
 
-    def eliminar_bd(self, nombre) -> None:
-        nombre = nombre
-        self.conectar_servidor()
-        if self.verificar_bd(nombre) == True:
-            self.cursor.execute(f"DROP DATABASE {nombre};")
+    def crearBD(self, baseDatos) -> None:
+        self.conectarServidor()
+        self.verificarBD(baseDatos)
+        self.cursor.execute(f"CREATE DATABASE IF NOT EXISTS {baseDatos};")
+
+    def crearUsuario(self, usuario, contraseña) -> None:
+        self.conectarServidor()
+        self.verificarUsuario(usuario)
+        self.cursor.execute(f"CREATE USER '{usuario}'@'%' IDENTIFIED BY '{contraseña}'")
+                            
+    def eliminarBD(self, baseDatos) -> None:
+        self.conectarServidor()
+        if self.verificarBD(baseDatos) == True:
+            self.cursor.execute(f"DROP DATABASE {baseDatos};")
             print("La base de datos se borró correctamente")
         else:
             print("No existe la base de datos")
@@ -52,11 +66,11 @@ class BasedeDatos:
     #     self.cursor.execute("SHOW DATABASES;")
     #     for mostrar in self.cursor:
     #         print(mostrar[0])
-    
+    1
     def generarContraseña(self) -> None:
         letras = string.ascii_letters
         numeros = string.digits
 
-
 bd = BasedeDatos()
-bd.crear_bd("ERIK")
+bd.conectarServidor()
+bd.verificarBD("erik")
